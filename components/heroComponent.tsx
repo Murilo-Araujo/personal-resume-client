5843fee57bdc4675adc5fb6c02235483bedc0c45
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2023-09-18 23:51:09 -0300

    first commit

---
+++ components/heroComponent.tsx
@@ -0,0 +1,27 @@
+interface HeroComponentProps {
+    title: string;
+    subtitle: string;
+}
+
+function HeroComponent(props: HeroComponentProps) {
+    return (
+        // colocando um css para que quando a imagem ultrapassar a direita, ela não apareça
+        <div className='h-screen bg-black w-full relative overflow-hidden'>
+            <div className="w-full h-full flex justify-center items-center absolute">
+                <div className=" md:mb-24 md:ml-16">
+                     {/*eslint-disable-next-line react/no-unescaped-entities */}
+                    <h1 className="text-white z-50 text-[25px] md:text-[60px]">HELLO THERE, I'M</h1>
+                    <h1 className="text-white z-50 text-[40px] md:text-[90px] font-extrabold">MURILO MARÇAL</h1>
+                    <h1 className="text-white z-50 text-[25px] md:text-[60px]">A FULLSTACK DEVELOPER</h1>
+                </div>
+            </div>
+
+            <img src="/a.png" className="w-full h-full abosolute"/>
+            <img src="/cloud2.png" className="md:h-1/2 h-[30%] absolute bottom-0 md:-right-36 -right-24 top-0 rounded-full opacity-80 scale-7"/>
+            <img src="/cloud.png" className="md:h-1/2 h-[30%] absolute bottom-0 md:-right-24 -left-24 rounded-full"/>
+            <img src="/me-in-astronaut.png" className='md:h-1/2 h-2/6 absolute bottom-4 left-5 astronaut-animation'/>
+        </div>
+    )
+}
+
+export default HeroComponent;
