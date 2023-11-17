d334c65084d999ea7fcabdda77d0bc0678cd02c8
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2023-11-17 02:16:49 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,13 @@
 import {Html, Head, Main, NextScript} from 'next/document'
+import HeaderComponent from "@/components/headerComponent";
+import ChangeLocaleComponent from "@/components/changeLocaleComponent";
 
 export default function Document() {
 
     return (
         <Html lang="en">
-            <Head>
+            <Head title={'Murilo Marçal de Araujo'}>
                 <link rel="icon" href="/me-with-glass.png"/>
-                <title>Murilo Marçal de Araujo</title>
             </Head>
             <body>
             <Main/>
