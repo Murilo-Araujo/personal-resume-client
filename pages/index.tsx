3c0a5f15104b4698bf7c4b50062a7ef8ebe87ad9
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2023-11-17 02:38:29 -0300

    feat: adding authorization token to github api

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,24 +6,30 @@ import ProjectsComponent from "@/components/projectsComponent";
 
 const inter = Inter({subsets: ['latin']})
 
-export default function Home( {projects}: any ) {
-
+export default function Home({projects}: any) {
 
 
     return (
         <>
             <HeroComponent/>
             <SummaryComponent/>
-            {/*<ProjectsComponent projects={projects}/>*/}
+            <ProjectsComponent projects={projects}/>
         </>
     )
 }
 
 export const getStaticProps = async () => {
-    const res = await fetch('https://api.github.com/users/murilo-araujo/repos')
+    const res =
+        await fetch('https://api.github.com/users/murilo-araujo/repos',
+            {
+                headers: {
+                    Authorization: `token ${process.env.GITHUB_TOKEN}`
+                }
+            })
     const projects = await res.json()
+
     return {
-        props: { projects: projects },
+        props: {projects: projects},
         revalidate: 60
     }
 }
