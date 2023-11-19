02e5af3c69f1f44c74d1754c16901921f22ea393
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2023-11-19 20:59:26 -0300

    contact pages

---
+++ pages/contacts.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen bg-black text-white">
+            <h1 className="text-4xl font-bold">Contact</h1>
+            <p className="text-xl"> This is the contact page</p>
+        </div>
+    )
+}
