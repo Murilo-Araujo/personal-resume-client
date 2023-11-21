64c956242cebf64e81740166894591595a575a75
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2023-11-20 23:02:06 -0300

    fixing translations

--- components/contactComponent.tsx
+++ components/contactComponent.tsx
@@ -1,5 +1,5 @@
 import BubbleComponent from "@/components/bubbleComponent";
-import {useTranslation} from "next-i18next";
+import {useTranslation} from "react-i18next";
 import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 export default function ContactComponent() {
