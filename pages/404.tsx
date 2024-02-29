6d0798486d66d4bebfd0a339558fb37ce5f3e66e
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2024-02-29 11:50:50 -0300

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
 
-function Error({statusCode}: any) {
+function Error404() {
     return (
 
         <>
@@ -9,7 +8,7 @@ function Error({statusCode}: any) {
                  className={'absolute top-0 left-0 w-full h-screen'}/>
             <div className="text-center absolute flex flex-col space-y-4 justify-center items-center w-full h-full">
                 <h1 className="text-6xl font-extrabold font-vela-sans text-white">Oops! Page not found</h1>
-                <h1 className="text-6xl font-extrabold font-vela-sans text-white ">{statusCode}</h1>
+                <h1 className="text-6xl font-extrabold font-vela-sans text-white ">404</h1>
                 <Link href={'/'}>
                     <div className="text-xl font-bold font-vela-sans text-blue-500 hover:text-blue-700 underline">Go back to home</div>
                 </Link>
@@ -18,9 +17,4 @@ function Error({statusCode}: any) {
     )
 }
 
-Error.getInitialProps = ({res, err}: any) => {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-    return {statusCode}
-}
-
-export default Error
+export default Error404
