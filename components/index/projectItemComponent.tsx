599be1bd93805136036fc08a3780618058118698
Murilo Marçal de Araujo <102617690+Murilo-Araujo@users.noreply.github.com>
2025-05-06 13:57:22 -0300

    feat: removing link

--- components/index/projectItemComponent.tsx
+++ components/index/projectItemComponent.tsx
@@ -15,7 +15,7 @@ export default function ProjectItemComponent({project}: ProjectItemComponentProp
     const {t} = useTranslation('projectItemComponent')
 
     return (
-        <Link href={`/project/${project.id}`} className="h-60 p-6 snap-center rounded-lg shadow border-4 border-black my-4 group cursor-pointer -wave_animation hover:shadow-lg w-96 hover:bg-black hover:text-white transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100">
+        <div className="h-60 p-6 snap-center rounded-lg shadow border-4 border-black my-4 group -wave_animation hover:shadow-lg w-96 hover:bg-black hover:text-white transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100">
             <div className="flex justify-center items-center mb-3">
                 <img src={`/icons/${project.language}.png`} className="w-10 h-10"/>
             </div>
@@ -37,6 +37,6 @@ export default function ProjectItemComponent({project}: ProjectItemComponentProp
                 <h5 className="mb-2 text-2xl font-semibold tracking-tight group-hover:underline hover:text-gray-400">{project.name}</h5>
             </div>
             <p className=" font-normal dark:text-gray-400">{project.description}</p>
-        </Link>
+        </div>
     )
 }
